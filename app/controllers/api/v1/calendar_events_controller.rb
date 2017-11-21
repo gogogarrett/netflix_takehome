8e02228aee3e7fc15b53f66006f607cc490f94b4
Garrett Heinlen <heinleng@gmail.com>
2017-11-21 20:02:26 +1100

    Hooking up the API to return data for the valid calendar_events.

--- app/controllers/api/v1/calendar_events_controller.rb
+++ app/controllers/api/v1/calendar_events_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::CalendarEventsController < ApplicationController
   def index
-    render json: { data: [] }
+    render json: {
+      data: Query::CalendarEvents.all_with_data
+    }
   end
 end
--- spec/controllers/api/v1/calendar_events_controller_spec.rb
+++ spec/controllers/api/v1/calendar_events_controller_spec.rb
@@ -11,5 +11,39 @@ RSpec.describe Api::V1::CalendarEventsController, type: :controller do
         expect(response.status).to eq 200
       end
     end
+
+    context "with valid events" do
+      let!(:missing_info_movie) do
+        CalendarEvent.create(
+          movie_id: "movie_1",
+          title: "The Matrix",
+          launch_date: nil,
+        )
+      end
+      let!(:valid_movie) do
+        CalendarEvent.create(
+          movie_id: "movie_2",
+          title: "Battlestar Galactica",
+          thumbnail_url: "https://i.imgur.com/Tgs8iJD.gif",
+          launch_date: 1493337600,
+        )
+      end
+
+      it 'returns calendar_events for the client' do
+        get :index, format: :json
+        json = JSON.parse(response.body)
+
+        expect(json['data'].size).to eq(1)
+        expect(json['data']).to include(hash_including(
+          {
+            "movie_id" => "movie_2",
+            "title" => "Battlestar Galactica",
+            "launch_date" => 1493337600,
+            "thumbnail_url" => "https://i.imgur.com/Tgs8iJD.gif",
+          },
+        ))
+        expect(response.status).to eq 200
+      end
+    end
   end
 end
