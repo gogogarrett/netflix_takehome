79f76b35d3600bbd2c754a2e4d2984811cf7ea73
Garrett Heinlen <heinleng@gmail.com>
2017-11-21 19:13:48 +1100

    Adding the title fetcher to the supervision tree.

--- lib/hydration_supervisor.rb
+++ lib/hydration_supervisor.rb
@@ -1,4 +1,5 @@
 class HydrationSupervisor < Celluloid::SupervisionGroup
   supervise ::Actor::DateFetcher, as: :date_fetcher, args: [5]
   supervise ::Actor::ThumbnailFetcher, as: :thumbnail_fetcher, args: [5]
+  supervise ::Actor::TitleFetcher, as: :title_fetcher, args: [5]
 end
