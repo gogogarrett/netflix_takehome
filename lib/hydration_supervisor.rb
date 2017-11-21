d3be48850e352200342b4af47a85867f3e1c3648
Garrett Heinlen <heinleng@gmail.com>
2017-11-21 18:57:41 +1100

    open/close - adding a new actor to the supervision group

--- lib/hydration_supervisor.rb
+++ lib/hydration_supervisor.rb
@@ -1,3 +1,4 @@
 class HydrationSupervisor < Celluloid::SupervisionGroup
   supervise ::Actor::DateFetcher, as: :date_fetcher, args: [5]
+  supervise ::Actor::ThumbnailFetcher, as: :thumbnail_fetcher, args: [5]
 end
