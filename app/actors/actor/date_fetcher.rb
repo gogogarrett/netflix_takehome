7a12999199a3dd0ab1f2c8719a8ebcaa206f0fae
Garrett Heinlen <heinleng@gmail.com>
2017-11-21 17:13:47 +1100

    Ensure we can properly fetch the data leveraging the HttpClient and handle bad status/json.

--- app/actors/actor/date_fetcher.rb
+++ app/actors/actor/date_fetcher.rb
@@ -8,6 +8,27 @@ module Actor
 
     def run
       Rails.logger.debug "polling dates"
+
+      dates_data
+    end
+
+    private
+
+    def dates_data
+      response_data = HttpClient.get(dates_url, headers)
+      parse_data(response_data)
+    end
+
+    def parse_data(response_data)
+      response_data && response_data["data"] || []
+    end
+
+    def dates_url
+      Rails.configuration.api_config.dates_service[:url]
+    end
+
+    def headers
+      { 'Authorization' => Rails.configuration.api_config.dates_service[:auth_token] }
     end
   end
 end
--- spec/actors/actor/date_fetcher_spec.rb
+++ spec/actors/actor/date_fetcher_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
+file = File.read(Rails.root.join("spec/support/dates.json"))
+data_hash = JSON.parse(file)
+
 RSpec.describe Actor::DateFetcher do
   it "returns a celluloid actor proxy" do
     actor = Actor::DateFetcher.new
@@ -14,5 +17,37 @@ RSpec.describe Actor::DateFetcher do
     # Not ideal, but still test the function is called every n seconds.
     expect(Rails).to receive(:logger).at_least(:twice).and_call_original
     sleep(3)
+  end
+
+  describe "fetching data from API" do
+    context "valid api response" do
+      before do
+        allow(HttpClient).to receive(:get).and_return(data_hash)
+      end
+
+      it "returns parsed" do
+        actor = Actor::DateFetcher.new(1)
+        dates = actor.run
+
+        expect(dates.size).to eq(11)
+        expect(dates.first["launch_date"]).to eq("2017-04-28 00:00:00")
+      end
+    end
+
+    context "invalid response" do
+      it "handles failure status, and returns an empty list" do
+        allow(HttpClient).to receive(:get).and_return({"status" => "failure"})
+        actor = Actor::DateFetcher.new(1)
+        dates = actor.run
+        expect(dates).to eq([])
+      end
+
+      it "handles nil, and returns an empty list" do
+        allow(HttpClient).to receive(:get).and_return(nil)
+        actor = Actor::DateFetcher.new(1)
+        dates = actor.run
+        expect(dates).to eq([])
+      end
+    end
   end
 end
