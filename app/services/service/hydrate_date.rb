35d7795a256fff6df9808b4b0f2a16e8da9dd5f5
Garrett Heinlen <heinleng@gmail.com>
2017-11-21 18:53:30 +1100

    Adding more information to the caller as to why this service failed

--- app/services/service/hydrate_date.rb
+++ app/services/service/hydrate_date.rb
@@ -6,13 +6,13 @@ module Service
     # with a unix timestamp of the launch date.
     #
     # Example input:
-    #   { "id": 1, "movie_id": "ee3c0801-9609-49ea-87fa-fcb9b9f438b9", "launch_date": "2017-04-28 00:00:00" },
+    #   { "id" => 1, "movie_id" => "ee3c0801-9609-49ea-87fa-fcb9b9f438b9", "launch_date" => "2017-04-28 00:00:00" },
     def call(movie_data)
       event = fetch_calendar_event(movie_data["movie_id"])
       if add_date_data(event, movie_data["launch_date"])
         [:success, event]
       else
-        [:error, nil]
+        [:error, "could not update the date for the movie_id"]
       end
     end
 
